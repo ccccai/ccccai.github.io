5baf7f6c3cbde2246a8cdd8465e53cf9e7a94ff9
caicai <1144164505@qq.com>
2023-11-06 19:00:52 +0800

    feature: 博客改版

---
+++ assets/index-2bf5eea2.js
@@ -0,0 +1,114 @@
+import{z,y as G,m as $,x as I,E as A}from"./index-9dda85be.js";const j=`{
+    "id": 1,
+    "title": "终于把这个编辑器引进来了",
+    "subTitle": "不是我说，作者能不能把文档写好一点，看源码太痛苦了。还有变量名也是取得奇奇怪怪。。",
+    "link": "http://localhost:3000/",
+    "description": "吃了凉菜嘴巴臭臭的。。。","tagIds": "23,21,20,19",
+    "author": "greeny11",
+    "authorAvatar": "https://img.zcool.cn/community/0170d259a805eea8012028a995cd12.jpg@1280w_1l_2o_100sh.jpg",
+    "weight": 7,
+    "type": 1,
+    "groupTimestamp": "1695223174272",
+    "cover": "https://img.zcool.cn/community/0170d259a805eea8012028a995cd12.jpg@1280w_1l_2o_100sh.jpg",
+    "categoryId": 3,
+    "tagList": [
+        {
+            "id": 19,
+            "name": "Mysql"
+        },
+        {
+            "id": 20,
+            "name": "PHP"
+        },
+        {
+            "id": 21,
+            "name": "px2rem"
+        },
+        {
+            "id": 23,
+            "name": "component"
+        }
+    ],
+    "createDate": "2023-09-20",
+    "content": "# useImperativeHandle\\n\\n\\n> useImperativeHandle：可以配合 forwardRef 自定义暴露给父组件的实例值。\\n\\n\\n*useImperativeHandle为我们提供了一个类似实例的东西*，它帮助我们通过useImperativeHandle 的第二个参数，将所返回的对象的内容挂载到父组件的 ref.current 上.\\n\\n\\n* [ ] useImperativeHandle 接收**三个参数**：\\n\\n\\n* ① 第一个参数 ref：接收 forWardRef 传递过来的 ref。\\n* ② ~~第二个参数 createHan...~~"
+    
+}`,E=`{
+    "id": 6,
+    "title": "使用异步函数",
+    "subTitle": "是短发都是发发呆改好先",
+    "link": "http://localhost:3000/",
+    "description": "少时诵诗书所所所",
+    "content": "> AAAAAAssss111\\n***\\n\\n**咱就是说，好想进啊**\\n* [ ] 加油呗",
+    "tagIds": "6",
+    "author": "好好改造",
+    "authorAvatar": "https://gss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/f9198618367adab4852e139289d4b31c8601e461.jpg",
+    "weight": 1,
+    "type": 1,
+    "groupTimestamp": "1604419200000",
+    "cover": "https://wallpaperm.cmcm.com/90370c9b7fba347f659bdc18b2a5eae2.jpg",
+    "categoryId": 3,
+    "tagList": [
+        {
+            "id": 6,
+            "name": "Git"
+        }
+    ],
+    "createDate": "2020-11-04"
+}`,J=`{
+    "id": 1,
+    "title": "终于把这个编辑器引进来了",
+    "subTitle": "不是我说，作者能不能把文档写好一点，看源码太痛苦了。还有变量名也是取得奇奇怪怪。。",
+    "link": "http://localhost:3000/",
+    "description": "吃了凉菜嘴巴臭臭的。。。","tagIds": "23,21,20,19",
+    "author": "greeny11",
+    "authorAvatar": "https://img.zcool.cn/community/0170d259a805eea8012028a995cd12.jpg@1280w_1l_2o_100sh.jpg",
+    "weight": 7,
+    "type": 1,
+    "groupTimestamp": "1695223174272",
+    "cover": "https://img.zcool.cn/community/0170d259a805eea8012028a995cd12.jpg@1280w_1l_2o_100sh.jpg",
+    "categoryId": 3,
+    "tagList": [
+        {
+            "id": 19,
+            "name": "Mysql"
+        },
+        {
+            "id": 20,
+            "name": "PHP"
+        },
+        {
+            "id": 21,
+            "name": "px2rem"
+        },
+        {
+            "id": 23,
+            "name": "component"
+        }
+    ],
+    "createDate": "2023-09-20",
+    "content": "# useImperativeHandle\\n\\n\\n> useImperativeHandle：可以配合 forwardRef 自定义暴露给父组件的实例值。\\n\\n\\n*useImperativeHandle为我们提供了一个类似实例的东西*，它帮助我们通过useImperativeHandle 的第二个参数，将所返回的对象的内容挂载到父组件的 ref.current 上.\\n\\n\\n* [ ] useImperativeHandle 接收**三个参数**：\\n\\n\\n* ① 第一个参数 ref：接收 forWardRef 传递过来的 ref。\\n* ② ~~第二个参数 createHan...~~"
+    
+}`,V=`{
+    "id": 6,
+    "title": "使用异步函数",
+    "subTitle": "是短发都是发发呆改好先",
+    "link": "http://localhost:3000/",
+    "description": "少时诵诗书所所所",
+    "content": "> AAAAAAssss111\\n***\\n\\n**咱就是说，好想进啊**\\n* [ ] 加油呗",
+    "tagIds": "6",
+    "author": "好好改造",
+    "authorAvatar": "https://gss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/f9198618367adab4852e139289d4b31c8601e461.jpg",
+    "weight": 1,
+    "type": 1,
+    "groupTimestamp": "1604419200000",
+    "cover": "https://wallpaperm.cmcm.com/90370c9b7fba347f659bdc18b2a5eae2.jpg",
+    "categoryId": 3,
+    "tagList": [
+        {
+            "id": 6,
+            "name": "Git"
+        }
+    ],
+    "createDate": "2020-11-04"
+}`;var f=typeof globalThis<"u"&&globalThis||typeof self<"u"&&self||typeof global<"u"&&global||{},u={searchParams:"URLSearchParams"in f,iterable:"Symbol"in f&&"iterator"in Symbol,blob:"FileReader"in f&&"Blob"in f&&function(){try{return new Blob,!0}catch{return!1}}(),formData:"FormData"in f,arrayBuffer:"ArrayBuffer"in f};function W(e){return e&&DataView.prototype.isPrototypeOf(e)}if(u.arrayBuffer)var X=["[object Int8Array]","[object Uint8Array]","[object Uint8ClampedArray]","[object Int16Array]","[object Uint16Array]","[object Int32Array]","[object Uint32Array]","[object Float32Array]","[object Float64Array]"],Z=ArrayBuffer.isView||function(e){return e&&X.indexOf(Object.prototype.toString.call(e))>-1};function b(e){if(typeof e!="string"&&(e=String(e)),/[^a-z0-9\-#$%&'*+.^_`|~!]/i.test(e)||e==="")throw new TypeError('Invalid character in header field name: "'+e+'"');return e.toLowerCase()}function O(e){return typeof e!="string"&&(e=String(e)),e}function x(e){var t={next:function(){var r=e.shift();return{done:r===void 0,value:r}}};return u.iterable&&(t[Symbol.iterator]=function(){return t}),t}function c(e){this.map={},e instanceof c?e.forEach(function(t,r){this.append(r,t)},this):Array.isArray(e)?e.forEach(function(t){if(t.length!=2)throw new TypeError("Headers constructor: expected name/value pair to be length 2, found"+t.length);this.append(t[0],t[1])},this):e&&Object.getOwnPropertyNames(e).forEach(function(t){this.append(t,e[t])},this)}c.prototype.append=function(e,t){e=b(e),t=O(t);var r=this.map[e];this.map[e]=r?r+", "+t:t};c.prototype.delete=function(e){delete this.map[b(e)]};c.prototype.get=function(e){return e=b(e),this.has(e)?this.map[e]:null};c.prototype.has=function(e){return this.map.hasOwnProperty(b(e))};c.prototype.set=function(e,t){this.map[b(e)]=O(t)};c.prototype.forEach=function(e,t){for(var r in this.map)this.map.hasOwnProperty(r)&&e.call(t,this.map[r],r,this)};c.prototype.keys=function(){var e=[];return this.forEach(function(t,r){e.push(r)}),x(e)};c.prototype.values=function(){var e=[];return this.forEach(function(t){e.push(t)}),x(e)};c.prototype.entries=function(){var e=[];return this.forEach(function(t,r){e.push([r,t])}),x(e)};u.iterable&&(c.prototype[Symbol.iterator]=c.prototype.entries);function _(e){if(!e._noBody){if(e.bodyUsed)return Promise.reject(new TypeError("Already read"));e.bodyUsed=!0}}function S(e){return new Promise(function(t,r){e.onload=function(){t(e.result)},e.onerror=function(){r(e.error)}})}function K(e){var t=new FileReader,r=S(t);return t.readAsArrayBuffer(e),r}function Q(e){var t=new FileReader,r=S(t),s=/charset=([A-Za-z0-9_-]+)/.exec(e.type),o=s?s[1]:"utf-8";return t.readAsText(e,o),r}function ee(e){for(var t=new Uint8Array(e),r=new Array(t.length),s=0;s<t.length;s++)r[s]=String.fromCharCode(t[s]);return r.join("")}function B(e){if(e.slice)return e.slice(0);var t=new Uint8Array(e.byteLength);return t.set(new Uint8Array(e)),t.buffer}function H(){return this.bodyUsed=!1,this._initBody=function(e){this.bodyUsed=this.bodyUsed,this._bodyInit=e,e?typeof e=="string"?this._bodyText=e:u.blob&&Blob.prototype.isPrototypeOf(e)?this._bodyBlob=e:u.formData&&FormData.prototype.isPrototypeOf(e)?this._bodyFormData=e:u.searchParams&&URLSearchParams.prototype.isPrototypeOf(e)?this._bodyText=e.toString():u.arrayBuffer&&u.blob&&W(e)?(this._bodyArrayBuffer=B(e.buffer),this._bodyInit=new Blob([this._bodyArrayBuffer])):u.arrayBuffer&&(ArrayBuffer.prototype.isPrototypeOf(e)||Z(e))?this._bodyArrayBuffer=B(e):this._bodyText=e=Object.prototype.toString.call(e):(this._noBody=!0,this._bodyText=""),this.headers.get("content-type")||(typeof e=="string"?this.headers.set("content-type","text/plain;charset=UTF-8"):this._bodyBlob&&this._bodyBlob.type?this.headers.set("content-type",this._bodyBlob.type):u.searchParams&&URLSearchParams.prototype.isPrototypeOf(e)&&this.headers.set("content-type","application/x-www-form-urlencoded;charset=UTF-8"))},u.blob&&(this.blob=function(){var e=_(this);if(e)return e;if(this._bodyBlob)return Promise.resolve(this._bodyBlob);if(this._bodyArrayBuffer)return Promise.resolve(new Blob([this._bodyArrayBuffer]));if(this._bodyFormData)throw new Error("could not read FormData body as blob");return Promise.resolve(new Blob([this._bodyText]))}),this.arrayBuffer=function(){if(this._bodyArrayBuffer){var e=_(this);return e||(ArrayBuffer.isView(this._bodyArrayBuffer)?Promise.resolve(this._bodyArrayBuffer.buffer.slice(this._bodyArrayBuffer.byteOffset,this._bodyArrayBuffer.byteOffset+this._bodyArrayBuffer.byteLength)):Promise.resolve(this._bodyArrayBuffer))}else{if(u.blob)return this.blob().then(K);throw new Error("could not read as ArrayBuffer")}},this.text=function(){var e=_(this);if(e)return e;if(this._bodyBlob)return Q(this._bodyBlob);if(this._bodyArrayBuffer)return Promise.resolve(ee(this._bodyArrayBuffer));if(this._bodyFormData)throw new Error("could not read FormData body as text");return Promise.resolve(this._bodyText)},u.formData&&(this.formData=function(){return this.text().then(ne)}),this.json=function(){return this.text().then(JSON.parse)},this}var te=["CONNECT","DELETE","GET","HEAD","OPTIONS","PATCH","POST","PUT","TRACE"];function re(e){var t=e.toUpperCase();return te.indexOf(t)>-1?t:e}function m(e,t){if(!(this instanceof m))throw new TypeError('Please use the "new" operator, this DOM object constructor cannot be called as a function.');t=t||{};var r=t.body;if(e instanceof m){if(e.bodyUsed)throw new TypeError("Already read");this.url=e.url,this.credentials=e.credentials,t.headers||(this.headers=new c(e.headers)),this.method=e.method,this.mode=e.mode,this.signal=e.signal,!r&&e._bodyInit!=null&&(r=e._bodyInit,e.bodyUsed=!0)}else this.url=String(e);if(this.credentials=t.credentials||this.credentials||"same-origin",(t.headers||!this.headers)&&(this.headers=new c(t.headers)),this.method=re(t.method||this.method||"GET"),this.mode=t.mode||this.mode||null,this.signal=t.signal||this.signal||function(){if("AbortController"in f){var n=new AbortController;return n.signal}}(),this.referrer=null,(this.method==="GET"||this.method==="HEAD")&&r)throw new TypeError("Body not allowed for GET or HEAD requests");if(this._initBody(r),(this.method==="GET"||this.method==="HEAD")&&(t.cache==="no-store"||t.cache==="no-cache")){var s=/([?&])_=[^&]*/;if(s.test(this.url))this.url=this.url.replace(s,"$1_="+new Date().getTime());else{var o=/\?/;this.url+=(o.test(this.url)?"&":"?")+"_="+new Date().getTime()}}}m.prototype.clone=function(){return new m(this,{body:this._bodyInit})};function ne(e){var t=new FormData;return e.trim().split("&").forEach(function(r){if(r){var s=r.split("="),o=s.shift().replace(/\+/g," "),n=s.join("=").replace(/\+/g," ");t.append(decodeURIComponent(o),decodeURIComponent(n))}}),t}function se(e){var t=new c,r=e.replace(/\r?\n[\t ]+/g," ");return r.split("\r").map(function(s){return s.indexOf(`
+`)===0?s.substr(1,s.length):s}).forEach(function(s){var o=s.split(":"),n=o.shift().trim();if(n){var i=o.join(":").trim();try{t.append(n,i)}catch(l){console.warn("Response "+l.message)}}}),t}H.call(m.prototype);function p(e,t){if(!(this instanceof p))throw new TypeError('Please use the "new" operator, this DOM object constructor cannot be called as a function.');if(t||(t={}),this.type="default",this.status=t.status===void 0?200:t.status,this.status<200||this.status>599)throw new RangeError("Failed to construct 'Response': The status provided (0) is outside the range [200, 599].");this.ok=this.status>=200&&this.status<300,this.statusText=t.statusText===void 0?"":""+t.statusText,this.headers=new c(t.headers),this.url=t.url||"",this._initBody(e)}H.call(p.prototype);p.prototype.clone=function(){return new p(this._bodyInit,{status:this.status,statusText:this.statusText,headers:new c(this.headers),url:this.url})};p.error=function(){var e=new p(null,{status:200,statusText:""});return e.status=0,e.type="error",e};var oe=[301,302,303,307,308];p.redirect=function(e,t){if(oe.indexOf(t)===-1)throw new RangeError("Invalid status code");return new p(null,{status:t,headers:{location:e}})};var y=f.DOMException;try{new y}catch{y=function(t,r){this.message=t,this.name=r;var s=Error(t);this.stack=s.stack},y.prototype=Object.create(Error.prototype),y.prototype.constructor=y}function R(e,t){return new Promise(function(r,s){var o=new m(e,t);if(o.signal&&o.signal.aborted)return s(new y("Aborted","AbortError"));var n=new XMLHttpRequest;function i(){n.abort()}n.onload=function(){var a={statusText:n.statusText,headers:se(n.getAllResponseHeaders()||"")};o.url.startsWith("file://")&&(n.status<200||n.status>599)?a.status=200:a.status=n.status,a.url="responseURL"in n?n.responseURL:a.headers.get("X-Request-URL");var d="response"in n?n.response:n.responseText;setTimeout(function(){r(new p(d,a))},0)},n.onerror=function(){setTimeout(function(){s(new TypeError("Network request failed"))},0)},n.ontimeout=function(){setTimeout(function(){s(new TypeError("Network request timed out"))},0)},n.onabort=function(){setTimeout(function(){s(new y("Aborted","AbortError"))},0)};function l(a){try{return a===""&&f.location.href?f.location.href:a}catch{return a}}if(n.open(o.method,l(o.url),!0),o.credentials==="include"?n.withCredentials=!0:o.credentials==="omit"&&(n.withCredentials=!1),"responseType"in n&&(u.blob?n.responseType="blob":u.arrayBuffer&&(n.responseType="arraybuffer")),t&&typeof t.headers=="object"&&!(t.headers instanceof c||f.Headers&&t.headers instanceof f.Headers)){var h=[];Object.getOwnPropertyNames(t.headers).forEach(function(a){h.push(b(a)),n.setRequestHeader(a,O(t.headers[a]))}),o.headers.forEach(function(a,d){h.indexOf(d)===-1&&n.setRequestHeader(d,a)})}else o.headers.forEach(function(a,d){n.setRequestHeader(d,a)});o.signal&&(o.signal.addEventListener("abort",i),n.onreadystatechange=function(){n.readyState===4&&o.signal.removeEventListener("abort",i)}),n.send(typeof o._bodyInit>"u"?null:o._bodyInit)})}R.polyfill=!0;f.fetch||(f.fetch=R,f.Headers=c,f.Request=m,f.Response=p);const ie=(e="",t=null)=>{let r="";if(t)for(let s in t)r+=`${r?"&":"?"}${s}=${t[s]}`;return`${e}${r}`};var U={exports:{}};(function(e,t){(function(r,s){e.exports=s()})(z,function(){var r={LTS:"h:mm:ss A",LT:"h:mm A",L:"MM/DD/YYYY",LL:"MMMM D, YYYY",LLL:"MMMM D, YYYY h:mm A",LLLL:"dddd, MMMM D, YYYY h:mm A"};return function(s,o,n){var i=o.prototype,l=i.format;n.en.formats=r,i.format=function(h){h===void 0&&(h="YYYY-MM-DDTHH:mm:ssZ");var a=this.$locale().formats,d=function(C,P){return C.replace(/(\[[^\]]+])|(LTS?|l{1,4}|L{1,4})/g,function(ye,k,w){var Y=w&&w.toUpperCase();return k||P[w]||r[w]||P[Y].replace(/(\[[^\]]+])|(MMMM|MM|DD|dddd)/g,function(me,N,q){return N||q.slice(1)})})}(h,a===void 0?{}:a);return l.call(this,d)}}})})(U);var ae=U.exports;const ce=G(ae);$.extend(ce);var M=(e=>(e.GET="GET",e.POST="POST",e.PUT="PUT",e.PATCH="PATCH",e.DELETE="DELETE",e))(M||{});const fe="http://127.0.0.1:3232",le=["POST","PUT","PATCH","DELETE"];let T=0;function ue(){T===0&&!A().spinning&&A().setSpinning(!0),T++}function D(){T--,T===0&&A().spinning&&A().setSpinning(!1)}function L(e,t){const r={};return Object.keys(e).forEach(s=>{r[s]=e[s]}),r}function he(e,t){console.log("err",e);const r={500:{type:"exception",msg:"接口异常"},501:{type:"exception",msg:"接口异常"},502:{type:"exception",msg:"接口异常"},503:{type:"exception",msg:"接口异常"},401:{type:"noPrmission",msg:"没有访问权限"},403:{type:"refuse",msg:"接口拒绝访问"},404:{type:"notFound",msg:"接口链接不存在"},405:{type:"Method Not Allowed",msg:"server: 方法不允许"}};let s={};e&&e.status&&(s=r[Number(e.status)],e.status===408&&t&&t.abort()),I.error((s==null?void 0:s.msg)||"未知的异常")}class de{async fetchHandler(t,r,s){ue();let o=fe+t;const n=Object.assign({headers:{"Content-Type":"application/x-www-form-urlencoded",Accept:"application/json"},method:"GET",timeout:1e4,mode:"cors",cache:"no-cache",params:{timeStamp:new Date().getTime()}},r);s&&(n.signal=s.signal);const i=n&&n.method&&le.includes(n.method);n.params&&n.method&&(i?(n.body=JSON.stringify(L(n.params)),n.headers&&Reflect.set(n.headers,"Content-Type","application/json")):n.method==="GET"&&(o=ie(o,L(n.params)))),console.log("opts",n);try{const h=await(await Promise.race([fetch(o,n),new Promise((a,d)=>{setTimeout(()=>d({status:408,statusText:"请求超时，请稍后重试",url:t}),n.timeout)})])).json();return(h==null?void 0:h.resultCode)!==0&&I.error((h==null?void 0:h.resultMessage)||"接口请求出错"),await D(),h}catch(l){return D(),he(l,s),l}}}const{fetchHandler:pe}=new de,g=async e=>(await fetch(e)).json(),F=({url:e,param:t={},options:r={}})=>(t&&typeof t=="object"&&(r.params=t),r.method=M.POST,pe(e,r)),v="/json/",ge=async()=>await g(`${v}tags.json`),we=async()=>await g(`${v}categories.json`),Ae=async()=>await g(`${v}count.json`),Te=async()=>await g(`${v}blogInfo.json`),ve=async e=>await g(e),_e=()=>new Promise((e,t)=>{try{const r=Object.assign({"../../public/json/article/tech/2023/1.json":j,"../../public/json/article/tech/2023/6.json":E}),s=[];for(let o in r)r[o]&&s.push(JSON.parse(r[o]));e(s)}catch(r){t(r)}}),je=()=>new Promise((e,t)=>{try{const r=Object.assign({"../../public/json/article/tech/2023/1.json":j,"../../public/json/article/tech/2023/6.json":E}),s=[];let o=0;for(let n in r)if(o++,r[n]&&s.push(JSON.parse(r[n])),console.log(11111,s),o>=3)break;e(s)}catch(r){t(r)}}),Ee=()=>new Promise((e,t)=>{try{const r=[];let s,o=[{date:"2023",list:Object.assign({"../../public/json/article/tech/2023/1.json":j,"../../public/json/article/tech/2023/6.json":E})},{date:"2022",list:Object.assign({})},{date:"2021",list:Object.assign({})},{date:"2020",list:Object.assign({})},{date:"2019",list:Object.assign({})},{date:"2018",list:Object.assign({})}],n=0;o.forEach(i=>{if(n+=Object.keys(i.list).length,Object.keys(i.list).length){s={date:i.date,list:[]};for(let l in i.list)i.list[l]&&s.list.push(JSON.parse(i.list[l]));r.push(s)}}),e({total:n,list:r})}catch(r){t(r)}}),Oe=()=>new Promise((e,t)=>{try{const r=[];let s,o=[{date:"2023",list:Object.assign({"../../public/json/article/life/2023/1.json":J,"../../public/json/article/life/2023/6.json":V})},{date:"2022",list:Object.assign({})},{date:"2021",list:Object.assign({})},{date:"2020",list:Object.assign({})},{date:"2019",list:Object.assign({})},{date:"2018",list:Object.assign({})}],n=0;o.forEach(i=>{if(n+=Object.keys(i.list).length,Object.keys(i.list).length){s={date:i.date,list:[]};for(let l in i.list)i.list[l]&&s.list.push(JSON.parse(i.list[l]));r.push(s)}}),e({total:n,list:r})}catch(r){t(r)}}),xe=async e=>await F({url:"/admin/createArticle",param:e}),Pe=async e=>await F({url:"/admin/updateArticle",param:e});export{ge as a,we as b,Ae as c,_e as d,je as e,Te as f,Ee as g,Oe as h,xe as i,ve as j,v as p,Pe as u};
